bcf275cbfcfe8d57afa8e4be85e93b3f2e77acf8
Suebin Kim <k.sue3028@gmail.com>
2023-12-02 02:48:12 +0900

    feat: make chat main component

---
+++ src/components/ChatMain/ChatMain.styles.ts
@@ -0,0 +1,11 @@
+import { Layout } from "@design-system";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column-reverse;
+  border: 2px solid red;
+  overflow-y: scroll;
+`;
--- src/components/ChatMain/ChatMain.tsx
+++ src/components/ChatMain/ChatMain.tsx
@@ -1,5 +1,20 @@
+import { Layout as L } from "@design-system";
+import { useState } from "react";
+import Bubble from "./components/Bubble/Bubble";
+import * as S from "./ChatMain.styles";
+import TextInput from "./components/TextInput/TextInput";
 const ChatMain = () => {
-  return <></>;
+  const [step, setStep] = useState<"INTRO" | "UPLOAD_CV" | "BEH_Q">("INTRO");
+  return (
+    <L.FlexCol w={"100%"} h={"100%"}>
+      <S.Container>
+        <L.FlexRow>
+          <Bubble content={"wefwfew"} isMine={false} />
+        </L.FlexRow>
+      </S.Container>
+      <TextInput />
+    </L.FlexCol>
+  );
 };
 
 export default ChatMain;
