0549198fe0f29320526bae059c9041833c271b86
Suebin Kim <k.sue3028@gmail.com>
2024-08-03 04:02:08 +0900

    chore: remove deprecated components

--- src/_components/steps/Evaluation/Evaluation.utils.ts
+++
@@ -1,6 +0,0 @@
-import { InterviewTypes } from "@types";
-
-export const isScoreEval = (
-  evaluation: InterviewTypes.Evaluation
-): evaluation is InterviewTypes.ScoreEvaluation =>
-  "score" in evaluation && "rationale" in evaluation;
