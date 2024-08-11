489ab2fc1a9ce958cc100defc1eb374187979a9b
Suebin Kim <k.sue3028@gmail.com>
2024-08-12 04:58:27 +0900

    feat: add all event types

--- src/components/EventLogger.tsx
+++ src/components/EventLogger.tsx
@@ -18,17 +18,126 @@ class _EventLogger {
 }
 
 type EventParams = {
-  global_navigation_bar_button: {
-    action: "logo" | "서비스 안내" | "면접 연습" | "면접 결과" | "로그인";
+  global_navigation_bar_button:
+    | "logo"
+    | "서비스 안내"
+    | "면접 연습"
+    | "면접 결과"
+    | "로그인";
+  global_navigation_bar_profile: "프로필" | "로그아웃";
+  footer_button: "Service Guide" | "Private Policy" | "Survey";
+  Home: undefined;
+  home_main_banner_card: "면접 연습하러 가기";
+  home_banner_card: "지금 면접 시작하기";
+  LogIn: undefined;
+  login_button: undefined;
+  IdLogIn: undefined;
+  id_login_button: "로그인" | "아이디/비밀번호 찾기" | "회원가입";
+  SignUp: undefined;
+  signup_button: "인증번호 받기" | "확인" | "중복확인" | "회원가입";
+  InterviewSetting: undefined;
+  interview_setting_button: {
+    corp_name: string;
+    occupation_name: string;
+    job_name: string;
   };
+  interview_setting_popup_button: "다시 입력" | "면접 시작";
+  InterviewSettingLoading: undefined;
+  InterviewSettingConfirm: undefined;
+  interview_setting_confirm_button: string;
+  EnvironmentCheck01: undefined;
+  EnvironmentCheck02: undefined;
+  EnvironmentCheck03: undefined;
+  EnvironmentCheck04: undefined;
+  environment_check_button: "다시하기" | "입장하기";
+  WaitingRoom: undefined;
+  Interview: undefined;
+  interview_chat_button: undefined;
+  interview_exit_button: undefined;
+  interview_chat_send_button: string;
+  interview_exit_popup_button: {
+    action: "취소" | "면접 나가기";
+    popup_title: string;
+  };
+  interview_stop_popup_button: undefined;
+  InterviewFinishPopUp: { popup_title: string };
+  interview_finish_popup_button: { popup_title: string };
+  InterviewResult: undefined;
+  interview_result_card: {
+    corp_name: string;
+    occupation_name: string;
+    job_name: string;
+    score: number;
+    interview_datetime: string;
+  };
+  InterviewResultDetail: {
+    corp_name: string;
+    occupation_name: string;
+    job_name: string;
+    score: number;
+    interview_datetime: string;
+  };
+  interview_result_detail_card: {
+    // TODO
+    corp_name: string;
+    occupation_name: string;
+    job_name: string;
+    score: number;
+    interview_datetime: string;
+  };
+  Profile: undefined;
+  profile_button: string;
 };
 
 type EventKey = keyof EventParams;
 
+const actionOnly = (action: string) => ({ action });
+const noParam = () => ({});
+const labelOnly = (label: string) => ({ label });
+const attributesOnly = (attributes: Record<string, any>) => ({ attributes });
+
 const factory: { [k in EventKey]: (params: EventParams[k]) => EventProp } = {
-  global_navigation_bar_button: ({ action }) => ({
+  global_navigation_bar_button: actionOnly,
+  global_navigation_bar_profile: actionOnly,
+  footer_button: actionOnly,
+  Home: noParam,
+  home_main_banner_card: labelOnly,
+  home_banner_card: labelOnly,
+  LogIn: noParam,
+  IdLogIn: noParam,
+  login_button: noParam,
+  id_login_button: actionOnly,
+  SignUp: noParam,
+  signup_button: actionOnly,
+  InterviewSetting: noParam,
+  interview_setting_button: attributesOnly,
+  interview_setting_popup_button: actionOnly,
+  InterviewSettingLoading: noParam,
+  InterviewSettingConfirm: noParam,
+  interview_setting_confirm_button: labelOnly,
+  EnvironmentCheck01: noParam,
+  EnvironmentCheck02: noParam,
+  EnvironmentCheck03: noParam,
+  EnvironmentCheck04: noParam,
+  environment_check_button: actionOnly,
+  WaitingRoom: noParam,
+  Interview: noParam,
+  interview_chat_button: noParam,
+  interview_exit_button: noParam,
+  interview_chat_send_button: labelOnly,
+  interview_exit_popup_button: ({ action, popup_title }) => ({
     action,
+    label: popup_title,
   }),
+  interview_stop_popup_button: noParam,
+  InterviewFinishPopUp: ({ popup_title }) => ({ label: popup_title }),
+  interview_finish_popup_button: ({ popup_title }) => ({ label: popup_title }),
+  InterviewResult: noParam,
+  interview_result_card: attributesOnly,
+  InterviewResultDetail: attributesOnly,
+  interview_result_detail_card: attributesOnly, // TODO
+  Profile: noParam,
+  profile_button: labelOnly,
 };
 
 export const EventLogger = new _EventLogger();
