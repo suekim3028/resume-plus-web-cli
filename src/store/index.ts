b4050903175f90a19e0f0c34f7044d6fd36696be
Suebin Kim <k.sue3028@gmail.com>
2024-08-13 01:57:10 +0900

    chore: remove redundant stores

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,43 +4,6 @@ import { TokenStorage } from "@storage";
 import { InterviewTypes, UserTypes } from "@types";
 import { atom, selector } from "recoil";
 
-export const interviewInfoStore = atom<null | InterviewTypes.InterviewInfo>({
-  key: "interviewInfo",
-  default: null,
-});
-
-export const interviewQuestionsStore = selector({
-  key: "interviewQuestions",
-  get: async ({ get }) => {
-    const interviewInfo = get(interviewInfoStore);
-    if (!interviewInfo) return null;
-
-    // const { techQ, behavQ } = await interviewApis.getCommonQ();
-    // const { perQ } = await interviewApis.getPersonalQ();
-
-    // return { techQ, behavQ, perQ };
-  },
-});
-
-export const evaluationStore = atom<InterviewTypes.Feedback[]>({
-  key: "evaluation",
-  default: [],
-});
-
-export const evaluationFinished = selector({
-  key: "evaluationFinishedStore",
-  get: ({ get }) => {
-    const questions = get(interviewQuestionsStore);
-    const evaluation = get(evaluationStore);
-
-    if (!questions) return false;
-
-    // return [...questions.behavQ, ...questions.perQ, ...questions.techQ].every(
-    //   ({ id }) => !!evaluation.find(({ questionId }) => id === questionId)
-    // );
-  },
-});
-
 export const userStoreRefresher = atom<number>({
   key: "userRefresher",
   default: 0,
@@ -49,7 +12,6 @@ export const userStoreRefresher = atom<number>({
 export const userStore = selector<UserTypes.User | null>({
   key: "user",
   get: async ({ get }) => {
-    console.log("?????");
     get(userStoreRefresher);
     const token = TokenStorage.get();
 
