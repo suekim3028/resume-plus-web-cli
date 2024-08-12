dbff70e2fe528e570af047301fa4ea5eaeeedc4e
Suebin Kim <k.sue3028@gmail.com>
2024-08-12 15:53:07 +0900

    style: complete profile page ui

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -24,7 +24,7 @@ const ProfileComponent = () => {
     <TopBarContainer>
       <Flex py={116} w={"100%"} flexDir={"column"}>
         <GridWrapper w="100%" pb={48}>
-          <GridItem colStart={5}>
+          <GridItem colStart={5} colSpan={2}>
             <Text type={"Title1"} fontWeight={"700"}>
               프로필
             </Text>
@@ -34,21 +34,23 @@ const ProfileComponent = () => {
         <Row title={"이메일"} body={email} />
         <Row title={"기본 이력서"} body={defaultResume} />
         <Row title={"응시 잔여 횟수"} body={`${remainInterview}회`} />
-      </Flex>
-      <Flex direction={"column"} pt={40} alignItems={"center"}>
-        <Text
-          type={"Heading1"}
-          color={"Label/Alternative"}
-          fontWeight={"400"}
-          pb={16}
-        >
-          간단한 설문조사 후에 면접 연습 30회권을 받아가세요!
-        </Text>
-        <Button
-          type={"Outlined_Primary"}
-          size="Large"
-          title={"설문조사 하기"}
-        />
+        <Flex direction={"column"} pt={40} alignItems={"center"}>
+          <Text
+            type={"Heading1"}
+            color={"Label/Alternative"}
+            fontWeight={"400"}
+            pb={16}
+            pt={40}
+          >
+            간단한 설문조사 후에 면접 연습 30회권을 받아가세요!
+          </Text>
+          <Button
+            type={"Outlined_Primary"}
+            size="Large"
+            title={"설문조사 하기"}
+            href={""} // TODO: 링크 연결
+          />
+        </Flex>
       </Flex>
     </TopBarContainer>
   );
