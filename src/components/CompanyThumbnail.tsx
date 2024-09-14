57589cca291d21a029846fb7987e1ab23289617d
Suebin Kim <k.sue3028@gmail.com>
2024-09-15 01:00:24 +0900

    style: set company thumbnail style when no image

--- src/components/CompanyThumbnail.tsx
+++ src/components/CompanyThumbnail.tsx
@@ -3,22 +3,44 @@ import { Flex, Text } from "@uis";
 
 const CompanyThumbnail = ({
   thumbnailUrl,
-  companyName,
+  companyName: _companyName,
   size,
 }: Pick<InterviewTypes.Company, "thumbnailUrl" | "companyName"> & {
-  size: number;
+  size: "small" | "large";
 }) => {
+  const sizeN = size === "small" ? 48 : 128;
+  const rounded = size === "small" ? 12 : 10;
+
+  if (thumbnailUrl) {
+    return (
+      // eslint-disable-next-line @next/next/no-img-element, jsx-a11y/alt-text
+      <img
+        src={thumbnailUrl}
+        width={sizeN}
+        height={sizeN}
+        style={{ width: sizeN, height: sizeN, borderRadius: rounded }}
+      />
+    );
+  }
+
+  const companyName =
+    _companyName.length > 8 ? `${_companyName.slice(0,7)}...` : _companyName;
+
   return (
     <Flex
-      w={size}
-      h={size}
-      flexShrink={0}
-      rounded={10}
-      alignItems={"center"}
+      w={sizeN}
+      h={sizeN}
+      py={size === "small" ? 8 : 16}
+      px={size === "small" ? 2 : 0}
+      rounded={rounded}
       justifyContent={"center"}
-      bgRgbColor="black"
+      bgColor={"Fill/Strong"}
     >
-      <Text type={"Label1_Normal"} color={"Static/White"}>
+      <Text
+        type={size === "small" ? "Caption1" : "Title1"}
+        fontWeight={size === "small" ? "400" : "500"}
+        color={"Static/Black"}
+      >
         {companyName}
       </Text>
     </Flex>
