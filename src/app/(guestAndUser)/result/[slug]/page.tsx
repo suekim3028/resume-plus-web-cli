44744324f56768fff898bd1bc1b69822db8cd27c
Suebin Kim <k.sue3028@gmail.com>
2024-09-14 19:23:36 +0900

    refactor: handle server generated page

---
+++ src/app/(guestAndUser)/result/[slug]/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { GridItem } from "@chakra-ui/react";
+import { Icon } from "@components";
+
+import { Flex, GridWrapper, Text } from "@uis";
+import { useRouter } from "next/navigation";
+
+import { useCompanyData, useResult } from "@atoms";
+import { getScoreStat } from "../utils";
+import FeedbackList from "./components/FeedbackList";
+import InterviewInfoCard from "./components/InterviewInfoCard";
+import ScoreCalcInfo from "./components/ScoreCalcInfo";
+
+const ResultDetail = ({ params }: { params: { slug: number } }) => {
+  const interviewId = params.slug;
+
+  const resultInterviews = useResult();
+  const { getJobsByDepartmentId, ...companyData } = useCompanyData();
+  const router = useRouter();
+  const interview = resultInterviews?.done.find((i) => {
+    return i.interviewId === Number(interviewId);
+  });
+
+  if (!interview) return <></>;
+
+  const { companyId, jobId, departmentId, createdAt } = interview;
+
+  const company = companyData?.companies.find(
+    ({ companyId: id }) => id === companyId
+  );
+  const department = companyData?.departmentGroups.find(
+    ({ departmentId: id }) => id === departmentId
+  );
+
+  const job = department
+    ? getJobsByDepartmentId(department?.departmentId).find(
+        ({ companyJobId }) => companyJobId === jobId
+      ) || null
+    : null;
+
+  const { totalMean } = getScoreStat(interview);
+
+  return (
+    <Flex flexDir={"column"} alignItems={"center"} w="100%">
+      <GridWrapper>
+        <GridItem colSpan={2}>
+          <Flex gap={4} my={60} onClick={router.back} cursor={"pointer"}>
+            <Icon name="normalArrowLeft" size={16} />
+            <Text
+              type={"Label1_Normal"}
+              color={"Label/Alternative"}
+              fontWeight={"600"}
+            >
+              목록으로 돌아가기
+            </Text>
+          </Flex>
+        </GridItem>
+      </GridWrapper>
+      <GridWrapper pb={230}>
+        <GridItem colSpan={2}>
+          <Flex direction={"column"} w="100%" gap={24}>
+            <InterviewInfoCard
+              companyName={company?.companyName || ""}
+              jobName={job?.companyJob || ""}
+              departmentName={department?.department || ""}
+              meanScore={totalMean}
+              createdAt={createdAt}
+            />
+            <ScoreCalcInfo />
+          </Flex>
+        </GridItem>
+        <GridItem colSpan={10} colStart={3}>
+          <FeedbackList {...interview} />
+        </GridItem>
+      </GridWrapper>
+    </Flex>
+  );
+};
+
+export default ResultDetail;
--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -3,7 +3,7 @@ import { useLoadableUser } from "@atoms";
 import { UserTypes } from "@types";
 import { Flex } from "@uis";
 
-import React, { ReactNode, useEffect, useMemo, useRef } from "react";
+import React, { ReactNode, useMemo, useRef } from "react";
 
 const AuthLayout = ({
   children,
@@ -21,9 +21,8 @@ const AuthLayout = ({
   );
 
   const isValid = useMemo(() => {
-    if (typeof window === "undefined") return false; // static page generation issue
+    if (typeof window === "undefined") return true; // static page generation issue. 서버 컴포넌트 생성 시 overlay가 있는 상태에서 생성되면, 클라이언트에서 isValid값이 true가 초기값인 경우 서버 생성 그대로 보여주기 때문에 오버레이 안없어짐
     if (value.state !== "hasData") return false;
-    console.log(value.data?.loginType);
     const _isValid = validate(value.data);
 
     if (
@@ -37,38 +36,20 @@ const AuthLayout = ({
     return _isValid;
   }, [value.state === "hasData" ? value.data?.loginType : undefined]);
 
-  useEffect(() => {
-    if (typeof window === "undefined" || isValid) {
-      document.getElementsByClassName("noe").item(0)?.remove(); //reload 시 제대로 동작안하고 계속 overlay 남아 있는 문제. 직접 지워주는 거로 해결
-    }
-  }, []);
-
   return (
     <>
       {children}
-      {!isValid && (
-        <Flex
-          className="noe"
-          position={"fixed"}
-          left={0}
-          right={0}
-          top={0}
-          bottom={0}
-          bgRgbColor={"rgba(0,0,0,0.2)"}
-        >
-          hi
-        </Flex>
-      )}
+      <Flex
+        position={"fixed"}
+        left={0}
+        display={isValid ? "none" : "flex"}
+        right={0}
+        top={0}
+        bottom={0}
+        bgRgbColor={"rgba(0,0,0,0.2)"}
+      />
     </>
   );
 };
 
-const Test = () => {
-  console.log("!!");
-  return <></>;
-};
-
 export default React.memo(AuthLayout);
-// export default function Auth({ children }: { children: ReactNode }) {
-//   return children;
-// }
