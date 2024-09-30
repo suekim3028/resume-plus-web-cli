f5451a52f702c47ca3db413627c409e5028810be
Suebin Kim <k.sue3028@gmail.com>
2024-09-30 10:46:49 +0900

    feat: set ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-DSVKJPLVY4"
+        ></script>
+        <script
+          type={"text/javascript"}
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-DSVKJPLVY4');`,
+          }}
+        ></script>
+      </head>
       <body id="body">
         <GoogleOAuthProvider clientId={process.env.GOOGLE_OAUTH_CLIENT_KEY}>
           <Initializer>
