b4dd302bc1bcdcc34d63cc02a983481ffb73e2c4
Suebin Kim <k.sue3028@gmail.com>
2024-10-22 15:40:19 +0900

    refactor: separate find interview setting detail utils

--- src/utils/interview.utils.ts
+++ src/utils/interview.utils.ts
@@ -1,3 +1,5 @@
+import { InterviewTypes } from "@types";
+
 export const formatDate = (_date: string) => {
   console.log(_date);
 
@@ -6,4 +8,25 @@ export const formatDate = (_date: string) => {
   return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()} (${
     ["일", "월", "화", "수", "목", "금", "토"][date.getDay()]
   }) ${date.getHours()}:${date.getMinutes()}`;
+};
+
+export const findCompanyFromListById = (
+  companies: InterviewTypes.Company[],
+  companyId: number
+) => {
+  return companies.find(({ companyId: id }) => id === companyId);
+};
+
+export const findDepartmentFromListById = (
+  departments: InterviewTypes.Department[],
+  departmentId: number
+) => {
+  return departments.find(({ departmentId: id }) => id === departmentId);
+};
+
+export const findJobFromListById = (
+  jobs: InterviewTypes.Job[],
+  jobId: number
+) => {
+  return jobs.find(({ companyJobId: id }) => id === jobId);
 };
