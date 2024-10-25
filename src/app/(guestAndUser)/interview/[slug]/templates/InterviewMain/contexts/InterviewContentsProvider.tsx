f9f9956565ff61bce7a72f9e57fcf54c9baed7ec
Suebin Kim <k.sue3028@gmail.com>
2024-10-25 21:43:49 +0900

    refactor: separate interview contents only context

---
+++ src/app/(guestAndUser)/interview/[slug]/templates/InterviewMain/contexts/InterviewContentsProvider.tsx
@@ -0,0 +1,117 @@
+import { interviewApis } from "@apis";
+import assert from "assert";
+import {
+  createContext,
+  ReactNode,
+  useCallback,
+  useContext,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
+import { Subject, Subscription } from "rxjs";
+import { useInterviewDetailSetting } from "../../../contexts/InterviewDetailSettingContext";
+import { useInterviewQuestionsContext } from "../../../contexts/InterviewQuestionsContext";
+const InterviewContentsContext = createContext<InterviewContentsValue | null>(
+  null
+);
+
+type InterviewContentsValue = {
+  addQuestionEventListener: (cb: (question: string) => void) => Subscription;
+  addAnswerEventListener: (cb: (answer: string) => void) => Subscription;
+  answerToCurrentQuestion: (answer: string) => void;
+  startInterview: () => void;
+};
+
+const InterviewContentsProvider = ({ children }: { children: ReactNode }) => {
+  const { interviewId } = useInterviewDetailSetting();
+  const { sortedQuestionsWithType } = useInterviewQuestionsContext();
+  assert(sortedQuestionsWithType.length > 0);
+
+  const currentQuestionIndex = useRef(0);
+
+  const [$questionObserver] = useState(new Subject<string>());
+  const [$answerObserver] = useState(new Subject<string>());
+
+  const postAnswerAPIToCurrentQuestion = useCallback(
+    (answer: string) => {
+      const _currentQuestion =
+        sortedQuestionsWithType[currentQuestionIndex.current];
+      return interviewApis.answerQuestion({
+        type: _currentQuestion.type,
+        questionId: _currentQuestion.questionId,
+        answer,
+        interviewId,
+      });
+    },
+    [interviewId, sortedQuestionsWithType]
+  );
+
+  const broadcastCurrentQuestion = useCallback(() => {
+    $questionObserver.next(
+      sortedQuestionsWithType[currentQuestionIndex.current].question
+    );
+  }, [$questionObserver, sortedQuestionsWithType]);
+
+  const broadcastSubmittedAnswer = useCallback(
+    (answer: string) => $answerObserver.next(answer),
+    [$answerObserver]
+  );
+
+  const getAndBroadcastNextQuestion = useCallback(() => {
+    if (currentQuestionIndex.current === sortedQuestionsWithType.length) return; //TODO: status
+    currentQuestionIndex.current = currentQuestionIndex.current + 1;
+    broadcastCurrentQuestion();
+  }, [sortedQuestionsWithType, broadcastCurrentQuestion]);
+
+  const answerToCurrentQuestion = useCallback(
+    (answer: string) => {
+      postAnswerAPIToCurrentQuestion(answer);
+      broadcastSubmittedAnswer(answer);
+      getAndBroadcastNextQuestion();
+    },
+    [
+      postAnswerAPIToCurrentQuestion,
+      broadcastSubmittedAnswer,
+      getAndBroadcastNextQuestion,
+    ]
+  );
+
+  const startInterview = useCallback(() => {
+    if (currentQuestionIndex.current !== 0) return;
+    getAndBroadcastNextQuestion();
+  }, [getAndBroadcastNextQuestion]);
+
+  const providerValue: InterviewContentsValue = useMemo(
+    () => ({
+      addQuestionEventListener: $questionObserver.subscribe,
+      addAnswerEventListener: $answerObserver.subscribe,
+      answerToCurrentQuestion,
+      startInterview,
+    }),
+    [
+      $questionObserver,
+      $answerObserver,
+      answerToCurrentQuestion,
+      startInterview,
+    ]
+  );
+
+  return (
+    <InterviewContentsContext.Provider value={providerValue}>
+      {children}
+    </InterviewContentsContext.Provider>
+  );
+};
+
+export const useInterviewContents = () => {
+  const context = useContext(InterviewContentsContext);
+
+  if (!context)
+    throw new Error(
+      "[InterviewContentsContext] UseInterviewContents must be used in provider."
+    );
+  return context;
+};
+
+export default InterviewContentsProvider;
